38503bcbf139a7305376d285a8e55266ade8f278
Denis Dryk <dzianis.dryk@gmail.com>
2025-10-31 16:45:58 +0300

    build(core): add initial nextjs apps
    Add ProjectA app. Add ProjectB app. Setup turbo scripts.

---
+++ apps/project-b/app/page.tsx
@@ -0,0 +1,13 @@
+import { ProductCard } from '@packages/shared-ui';
+
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <h2>This is Project B</h2>
+      <ProductCard />
+    </main>
+  );
+};
+
+export default HomePage;
