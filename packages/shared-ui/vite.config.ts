d7e8057ccc01c307bb3245c790ad8021ea60e35e
Denis Dryk <dzianis.dryk@gmail.com>
2025-11-03 14:47:43 +0300

    build(packages): add tailwindcss to shared-ui

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 import path from 'node:path';
@@ -14,7 +15,13 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
   test: {
     projects: [
       {
