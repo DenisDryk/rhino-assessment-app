5588bfffecc5927e5a096697a214028806db219f
Denis Dryk <dzianis.dryk@gmail.com>
2025-11-05 23:11:03 +0300

    fix(project-a): update initial redirect

--- apps/project-a/next.config.ts
+++ apps/project-a/next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from 'next';
 
+import BrandConfig from './src/brand-config.json';
+
 const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
     remotePatterns: [new URL('https://cdn.dummyjson.com/**')],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: `/${BrandConfig.BRAND.MARKETS.DEFAULT}`,
+        permanent: false,
+      },
+    ];
   },
 };
 
