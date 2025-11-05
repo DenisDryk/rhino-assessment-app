720b6dca47b82dd1c0ef7488e26642be672fe3fc
Denis Dryk <dzianis.dryk@gmail.com>
2025-11-05 21:37:01 +0300

    style(shared-ui): update typography variants

--- packages/shared-ui/src/components/Typography/Typography.constants.tsx
+++ packages/shared-ui/src/components/Typography/Typography.constants.tsx
@@ -3,9 +3,9 @@ import type { TVariant } from './Typography.types';
 export const VARIANT_LIST = ['h1', 'h2', 'h3', 'h4', 'h5', 'p'] as const;
 
 export const VARIANTS: Record<TVariant, string> = {
-  h1: 'font-semibold text-[2rem] leading-[1.20] sm:text-[2.25rem] lg:text-[2.5rem]',
-  h2: 'font-semibold text-[1.25rem] leading-[1.20] sm:text-[1.375rem] lg:text-[1.5rem]',
-  h3: 'font-semibold text-[1rem] leading-[1.20] sm:text-[1.125rem] lg:text-[1.25rem]',
-  h4: 'font-semibold text-[0.875rem] leading-[1.20] sm:text-[0.938rem] lg:text-[1rem]',
-  p: 'font-normal text-[0.875rem] leading-[1.40] sm:text-[0.938rem] lg:text-[1rem]',
+  h1: 'font-semibold text-[2rem] leading-[1.20]',
+  h2: 'font-semibold text-[1.25rem] leading-[1.20]',
+  h3: 'font-semibold text-[1rem] leading-[1.20]',
+  h4: 'font-semibold text-[0.875rem] leading-[1.20]',
+  p: 'font-normal text-[0.875rem] leading-[1.40]',
 };
